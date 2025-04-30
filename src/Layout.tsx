ac00e52daf89a8878d7211f7287dac44cfdb0249
AvoAshkharikian <avo@frontcloudmd.com>
2025-04-30 15:23:00 -0700

    Update Layout.tsx

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -4,9 +4,9 @@ type LayoutProps = {
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className="bg-gray-100 min-h-screen">
+    <div className="bg-gray-900 min-h-screen text-white">
       <div className="container mx-auto px-4 md:px-8 lg:px-12 py-10">
-        <div className="relative bg-white rounded-lg shadow-lg p-6 md:p-10">
+        <div className="relative bg-gray-800 rounded-lg shadow-lg p-6 md:p-10">
           {children}
         </div>
       </div>
